7b6af3ffaffc17aee3603748cc5eb7f341795b98
reco_luan <recoluan@outlook.com>
2020-04-22 00:10:47 +0800

    feat: update theme version

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,12 +14,7 @@ module.exports = {
       { text: 'Contact', 
         icon: 'reco-message',
         items: [
-          { text: 'NPM', link: 'https://www.npmjs.com/~reco_luan', icon: 'reco-npm' },
-          { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' },
-          { text: '简书', link: 'https://www.jianshu.com/u/cd674a19515e', icon: 'reco-jianshu' },
-          { text: 'CSDN', link: 'https://blog.csdn.net/recoluan', icon: 'reco-csdn' },
-          { text: '博客圆', link: 'https://www.cnblogs.com/luanhewei/', icon: 'reco-bokeyuan' },
-          { text: 'WeChat', link: 'https://mp.weixin.qq.com/s/mXFqeUTegdvPliXknAAG_A', icon: 'reco-wechat' },
+          { text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github' }
         ]
       }
     ],
