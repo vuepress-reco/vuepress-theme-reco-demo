1cc2603af15ea07389c9a41d26000664cf47b406
reco_luan <recoluan@qq.com>
2024-04-27 22:33:34 +0800

    chore: update version

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,10 +1,13 @@
 import { defineUserConfig } from "vuepress";
-import type { DefaultThemeOptions } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
+import { viteBundler } from '@vuepress/bundler-vite'
+import { webpackBundler } from '@vuepress/bundler-webpack'
 
 export default defineUserConfig({
   title: "vuepress-theme-reco",
   description: "Just playing around",
+  bundler: viteBundler(),
+  // bundler: webpackBundler(),
   theme: recoTheme({
     style: "@vuepress-reco/style-default",
     logo: "/logo.png",
